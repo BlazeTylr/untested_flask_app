473a9cea3c38962e778dc7fb4bad9e521ef64ffc
balazs szabo <csikai.b@gmail.com>
2024-02-22 12:42:58 +0000

    test complete

--- tests/test_user_flask.py
+++ tests/test_user_flask.py
@@ -1,9 +1,9 @@
 import json
 
 def test_getting_data(web_client):
-    post_response = web_client.get('/users')
-    assert post_response.status_code == 200
-    assert json.loads(post_response.data.decode('utf-8')) == [
+    get_response = web_client.get('/users')
+    assert get_response.status_code == 200
+    assert json.loads(get_response.data.decode('utf-8')) == [
     {
         "id": 1,
         "username": "john"
@@ -17,7 +17,7 @@ def test_getting_data(web_client):
         "username": "alice"
     }]
 
-def test_example(web_client):
-    response = web_client.get("/users")
-    assert response.status_code == 200
-    assert len(response.json) == 3
+def test_get_user_not_found(web_client):
+    response = web_client.get("/users/4")
+    assert response.status_code == 404
+    assert  json.loads(response.data.decode('utf-8')) == {'error': 'User not found'}
